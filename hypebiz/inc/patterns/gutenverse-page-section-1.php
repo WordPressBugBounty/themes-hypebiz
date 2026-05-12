d36eb3c6f6a7ef827b5d1a028663ebbf47779eb8
Sean Murphy <sean@iamseanmurphy.com>
2026-05-12 05:25:42 +0000

    1.0.9

---
+++ hypebiz/inc/patterns/gutenverse-page-section-1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Gutenverse Page Section 1 content.
+ */
+return array(
+	'title'      => __( 'Gutenverse Page Section 1', 'hypebiz' ),
+	'categories' => array( 'hypebiz-gutenverse' ),
+	'content'    => '<!-- wp:gutenverse/post-content {"elementId":"guten-846Cfv"} -->
+	<div class="guten-element guten-post-content guten-846Cfv"></div>
+	<!-- /wp:gutenverse/post-content -->',
+);
